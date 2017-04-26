95bdede2b8ec78586c44cecf80afb79491a1cb56
Roman Korenchuk <ikorew@gmail.com>
2017-04-27 01:42:31 +0300

    normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var less = require('gulp-less');
 var path = require('path');
  
 gulp.task('less', function () {
-  return gulp.src('./src/less/**/*.less')
+  return gulp.src('./src/less/main.less')
     .pipe(less({
       paths: [ path.join(__dirname, 'less', 'includes') ]
     }))
