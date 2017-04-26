c9f7f5acf9a17588df129ed05422872c9ad9cbb5
Roman Korenchuk <ikorew@gmail.com>
2017-04-27 02:19:52 +0300

    gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,32 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
- 
+var autoprefixer = require('gulp-autoprefixer');
+var csso = require('gulp-csso');
+var sourcemaps = require('gulp-sourcemaps');
+
+
 gulp.task('less', function () {
-  return gulp.src('./src/less/main.less')
-    .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
-    }))
-    .pipe(gulp.dest('./dist/css'));
+	return gulp.src('./src/less/main.less')
+	.pipe(sourcemaps.init())
+	.pipe(less({
+		paths: [ path.join(__dirname, 'less', 'includes') ]
+	}))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions'],
+		cascade: false
+	}))
+	.pipe(csso())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('html', function () {
+	return gulp.src('./src/*.html')
+	.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('fonts', function () {
+	return gulp.src('./src/fonts/*.+(svg|ttf|woff)')
+	.pipe(gulp.dest('./dist/fonts'));
 });
